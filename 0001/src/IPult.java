017762eb10e50e1a52dfa5e7214a1c9f68b1d535
Dmitriy <streamlineddirudo@gmail.com>
2017-04-03 16:09:46 +0300

    change interface

--- 0001/src/IPult.java
+++ 0001/src/IPult.java
@@ -4,8 +4,8 @@ package src;
  * Created by dem on 03.04.2017.
  */
 public interface IPult {
-    void pressButtonVolumeUp();
-    void pressButtonVolumeDown();
-    void pressButtonBrightUp();
-    void pressButtonBrightDown();
+    void pressButtonUp();
+    void pressButtonDown();
+    void pressButton1();
+    void pressButton2();
 }
