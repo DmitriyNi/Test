a1cc0c7a92239928c35640d4904b7b90f1d63750
Dmitriy <streamlineddirudo@gmail.com>
2017-04-03 16:06:36 +0300

    add 2 interface

---
+++ 0001/src/IDisplay.java
@@ -0,0 +1,9 @@
+package src;
+
+/**
+ * Created by dem on 03.04.2017.
+ */
+public interface IDisplay {
+void changeVolume();
+void changeBright();
+}
---
+++ 0001/src/IPult.java
@@ -0,0 +1,11 @@
+package src;
+
+/**
+ * Created by dem on 03.04.2017.
+ */
+public interface IPult {
+    void pressButtonVolumeUp();
+    void pressButtonVolumeDown();
+    void pressButtonBrightUp();
+    void pressButtonBrightDown();
+}
